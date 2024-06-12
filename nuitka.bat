3d0d409699d398c6bdcb87ee9bc7475830875123
Juergen Pfundt <juergen.pfundt@gmail.com>
2024-06-12 08:50:45 +0200

    windows version update

--- nuitka.bat
+++ nuitka.bat
@@ -1,1 +1,1 @@
-python -m nuitka--onefile --windows-console-mode=disable --enable-plugin=tk-inter  --windows-icon-from-ico=beck-view-digitize.png -o "beck-view-gui" beck_view_gui.py
+python -m nuitka  --windows-console-mode=disable --windows-icon-from-ico=beck-view-digitize.png -o "beck-view-gui" beck_view_gui.py
