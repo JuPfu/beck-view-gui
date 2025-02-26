485125976261c1a31e18de78b1b7ab87154aab07
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-02-26 12:14:35 +0100

    windows console mode properly disabled in nuitka.bat

--- nuitka.bat
+++ nuitka.bat
@@ -1,1 +1,1 @@
-python -m nuitka  --standalone --onefile --enable-plugin=tk-inter --disable-console --windows-icon-from-ico=beck-view-digitize.png -o "beck-view-gui" beck_view_gui.py
+python -m nuitka  --standalone --onefile --enable-plugin=tk-inter --windows-console-mode=disable --windows-icon-from-ico=beck-view-digitize.png -o "beck-view-gui" beck_view_gui.py
